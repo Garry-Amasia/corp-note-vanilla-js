a8a6184e42f85819465b46e3ce71780189b13a41
GarryAmasia <amasiagarry87@gmail.com>
2023-12-19 14:45:48 +1100

    adding some refactoring to the code(validation)

--- script.js
+++ script.js
@@ -1,49 +1,89 @@
 // --------------GLOBAL SELECTOR--------------
 const textareaEl = document.querySelector(".form__textarea");
 const charEl = document.querySelector(".counter");
-const orderedListEl = document.querySelector(".feedback");
+const orderedListEl = document.querySelector(".feedbacks");
 const formEl = document.querySelector(".form");
+const submitButtonEl = document.querySelector(".submit-btn");
+const MAX_CHAR = 150;
+
+const showVisualIndicator = (text) => {
+  const className = text === "valid" ? "form--valid" : "form--invalid";
+  console.log(`${className} runs`);
+  //show valid indicator
+  formEl.classList.add(className);
+  //remove visual indicator
+  setTimeout(() => formEl.classList.remove(className), 2000);
+};
 
 // --------------COUNTER COMPONENT--------------
 const inputHandler = () => {
   //get length of  characters from what user type
   const charLength = textareaEl.value.length;
-
   //calculate limit char
-  const charLeft = charEl.textContent - charLength;
-
+  const charLeft = MAX_CHAR - charLength;
   //visual indicator for charEl
   charEl.textContent = charLeft;
 };
+textareaEl.addEventListener("input", inputHandler);
 
-textareaEl.addEventListener("input", inputHandler());
-
-// --------------SUBMIT COMPONENT--------------
-
+// --------------FORM COMPONENT--------------
 const submitHandler = (e) => {
   //This will prevent reloading to the path action="#" in form html
   e.preventDefault();
-
   //get text from text area
   const text = textareaEl.value;
-
   // validation(must include hashtag for "company name" and text must be long enough)
   const validated = text.includes("#") && text.length > 10;
-  console.log(validated);
-
   if (validated) {
-    //show valid indicator
-    formEl.classList.add("form--valid");
-    //remove visual indicator
-    setTimeout(() => formEl.classList.remove("form--valid"), 2000);
+    showVisualIndicator("valid");
   } else {
-    //show invalid indicator
-    formEl.classList.add("form--invalid");
-    //remove invalid indicator
-    setTimeout(() => formEl.classList.remove("form--invalid"), 2000);
+    showVisualIndicator("invalid");
     //focus textarea again,user can continue typing again
     textareaEl.focus();
+    //stop function execution
+    return;
   }
+
+  //now we have the validated text, extract it and make it available as an information.
+  //get the company name
+  const hashtag = text.split(" ");
+  const wordContainHashTag = hashtag.find((item) => item.includes("#"));
+  const company = wordContainHashTag.substring(1);
+
+  //get batch letter character,upvote count and days ago
+  const batchLetter = company.substring(0, 1).toUpperCase();
+  const upvoteCount = 0;
+  const daysAgo = 0;
+
+  //create list in HTML format
+  const feedbackItemHTML = `
+  <li class="feedback">
+    <button class="upvote">
+        <i class="fa-solid fa-caret-up upvote__icon"></i>
+        <span class="upvote__count">${upvoteCount}</span>
+    </button>
+    <section class="feedback__badge">
+        <p class="feedback__letter">${batchLetter}</p>
+    </section>
+    <div class="feedback__content">
+        <p class="feedback__company">${company}</p>
+        <p class="feedback__text">${text}</p>
+    </div>
+    <p class="feedback__date">${daysAgo === 0 ? "new" : `${daysAgo}d`}</p>
+</li>`;
+
+  //insert feedback into the list
+  orderedListEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+
+  //   reset to its default(textarea="",count=0,unfocus button)
+  textareaEl.value = "";
+  charEl.textContent = MAX_CHAR;
+  submitButtonEl.blur();
 };
 
 formEl.addEventListener("submit", submitHandler);
+
+// --------------FEEDBACK COMPONENT--------------
+fetch("https://bytegrad.com/course-assets/js/1/api/feedbacks")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
